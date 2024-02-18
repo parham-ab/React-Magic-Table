a2e33607bb83f0e6b7b76f4a7d7337088bdea48f
Parham Abolghasemi <parhamab17@gmail.com>
2024-02-18 20:39:23 +0330

    save to localStorage

--- src/features/tableSlice.js
+++ src/features/tableSlice.js
@@ -1,15 +1,21 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
 
+const magic_table_data =
+  localStorage.getItem("magic-table-data") != null
+    ? JSON.parse(localStorage.getItem("magic-table-data"))
+    : [];
+
 export const tableSlice = createSlice({
   name: "crud-table",
   initialState: {
-    rows: [],
+    rows: magic_table_data,
   },
   reducers: {
     addRows: {
       reducer: (state, action) => {
         state.rows.push(action.payload);
         state.rows = state.rows.sort((a, b) => +a.priority - +b.priority);
+        localStorage.setItem("magic-table-data", JSON.stringify(state.rows));
       },
       prepare: (priority, name) => {
         return {
@@ -20,16 +26,17 @@ export const tableSlice = createSlice({
     deleteRows: (state, action) => {
       let filteredRows = state.rows.filter((row) => row.id !== action.payload);
       state.rows = filteredRows;
+      localStorage.setItem("magic-table-data", JSON.stringify(state.rows));
     },
     editRows: (state, action) => {
       const { id, priority, name } = action.payload;
       const currentTodo = state.rows.find((item) => item.id === id);
-      // console.log(currentTodo)
       if (currentTodo) {
         currentTodo.priority = priority;
         currentTodo.name = name;
       }
       state.rows = state.rows.sort((a, b) => +a.priority - +b.priority);
+      localStorage.setItem("magic-table-data", JSON.stringify(state.rows));
     },
   },
 });
